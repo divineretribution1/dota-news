bf6f3ea99de6ef0305b26d2c4846a1c8f8f4e174
Vladyslav Prus <vlpr@opinov8.com>
2022-09-27 14:25:31 +0300

    example

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import {NavBar} from './components'
+function App() {
+  return (
+    <>
+    <NavBar/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,6 @@
+import {Title} from './NavBar.style'
+export const NavBar = ({ }) => {
+    return (
+     <Title>123</Title>   
+    )
+}
